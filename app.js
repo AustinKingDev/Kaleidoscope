0b629a92c11260c0b568128458532cc2384235c8
Austin King <austintriesemail@gmail.com>
2020-12-02 20:47:49 -0600

    updated

---
+++ app.js
@@ -0,0 +1,18 @@
+const topLeftImage = document.getElementById("image0");
+const topRightImage = document.getElementById("image1");
+const bottomLeftImage = document.getElementById("image2");
+const bottomRightImage = document.getElementById("image3");
+
+const upBtn = document.getElementById("upBtn");
+const rightBtn = document.getElementById("rightBtn");
+const leftBtn = document.getElementById("leftBtn");
+const downBtn = document.getElementById("downBtn");
+console.log(upBtn)
+
+upBtn.addEventListener("click", function () {
+   
+topLeftImage.style.background-position += 0px 03px
+   
+})
+
+console.log(topLeftImage.style.backgroundPositionY)
