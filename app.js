82e868611218f7fc7d164d33a56682b3bcb09cd2
Austin King <austintriesemail@gmail.com>
2020-12-04 23:22:59 -0600

    Added new buttons

--- app.js
+++ app.js
@@ -9,7 +9,10 @@ const rightBtn = document.getElementById("rightBtn");
 const leftBtn = document.getElementById("leftBtn");
 const downBtn = document.getElementById("downBtn");
 const enterBtn = document.getElementById("enterBtn");
-
+const upLeftBtn = document.getElementById("upLeftBtn");
+const upRightBtn = document.getElementById("upRightBtn");
+const downLeftBtn = document.getElementById("downLeftBtn");
+const downRightBtn = document.getElementById("downRightBtn");
 
 
 
@@ -90,6 +93,134 @@ rightBtn.addEventListener("click", function () {
     let bottomRightX = window.getComputedStyle(bottomRightImage).getPropertyValue("background-position-x");
     let addPixel3 = parseInt(bottomRightX, 10) + 5 + "px";
     bottomRightImage.style.backgroundPositionX = addPixel3;
+  
+});
+
+upLeftBtn.addEventListener("click", function () {
+   
+    let topLeftY = window.getComputedStyle(topLeftImage).getPropertyValue("background-position-y");
+    let topLeftX = window.getComputedStyle(topLeftImage).getPropertyValue("background-position-x");
+    let addPixelY = parseInt(topLeftY, 10) - 5 + "px";
+    let addPixelX = parseInt(topLeftX, 10) - 5 + "px";
+    topLeftImage.style.backgroundPositionY = addPixelY;
+    topLeftImage.style.backgroundPositionX = addPixelX;
+
+    let topRightY = window.getComputedStyle(topRightImage).getPropertyValue("background-position-y");
+    let topRightX = window.getComputedStyle(topRightImage).getPropertyValue("background-position-x");
+    let addPixelY1 = parseInt(topRightY, 10) - 5 + "px";
+    let addPixelX1 = parseInt(topRightX, 10) - 5 + "px";
+    topRightImage.style.backgroundPositionY = addPixelY1;
+    topRightImage.style.backgroundPositionX = addPixelX1;
+
+    let bottomLeftY = window.getComputedStyle(bottomLeftImage).getPropertyValue("background-position-y");
+    let bottomLeftX = window.getComputedStyle(bottomLeftImage).getPropertyValue("background-position-x");
+    let addPixelY2 = parseInt(bottomLeftY, 10) - 5 + "px";
+    let addPixelX2 = parseInt(bottomLeftX, 10) - 5 + "px";
+    bottomLeftImage.style.backgroundPositionY = addPixelY2;
+    bottomLeftImage.style.backgroundPositionX = addPixelX2;
+
+    let bottomRightY = window.getComputedStyle(bottomRightImage).getPropertyValue("background-position-y");
+    let bottomRightX = window.getComputedStyle(bottomRightImage).getPropertyValue("background-position-x");
+    let addPixelY3 = parseInt(bottomRightY, 10) - 5 + "px";
+    let addPixelX3 = parseInt(bottomRightX, 10) - 5 + "px";
+    bottomRightImage.style.backgroundPositionY = addPixelY3;
+    bottomRightImage.style.backgroundPositionX = addPixelX3;
+  
+});
+
+upRightBtn.addEventListener("click", function () {
+   
+    let topLeftY = window.getComputedStyle(topLeftImage).getPropertyValue("background-position-y");
+    let topLeftX = window.getComputedStyle(topLeftImage).getPropertyValue("background-position-x");
+    let addPixelY = parseInt(topLeftY, 10) - 5 + "px";
+    let addPixelX = parseInt(topLeftX, 10) + 5 + "px";
+    topLeftImage.style.backgroundPositionY = addPixelY;
+    topLeftImage.style.backgroundPositionX = addPixelX;
+
+    let topRightY = window.getComputedStyle(topRightImage).getPropertyValue("background-position-y");
+    let topRightX = window.getComputedStyle(topRightImage).getPropertyValue("background-position-x");
+    let addPixelY1 = parseInt(topRightY, 10) - 5 + "px";
+    let addPixelX1 = parseInt(topRightX, 10) + 5 + "px";
+    topRightImage.style.backgroundPositionY = addPixelY1;
+    topRightImage.style.backgroundPositionX = addPixelX1;
+
+    let bottomLeftY = window.getComputedStyle(bottomLeftImage).getPropertyValue("background-position-y");
+    let bottomLeftX = window.getComputedStyle(bottomLeftImage).getPropertyValue("background-position-x");
+    let addPixelY2 = parseInt(bottomLeftY, 10) - 5 + "px";
+    let addPixelX2 = parseInt(bottomLeftX, 10) + 5 + "px";
+    bottomLeftImage.style.backgroundPositionY = addPixelY2;
+    bottomLeftImage.style.backgroundPositionX = addPixelX2;
+
+    let bottomRightY = window.getComputedStyle(bottomRightImage).getPropertyValue("background-position-y");
+    let bottomRightX = window.getComputedStyle(bottomRightImage).getPropertyValue("background-position-x");
+    let addPixelY3 = parseInt(bottomRightY, 10) - 5 + "px";
+    let addPixelX3 = parseInt(bottomRightX, 10) + 5 + "px";
+    bottomRightImage.style.backgroundPositionY = addPixelY3;
+    bottomRightImage.style.backgroundPositionX = addPixelX3;
+  
+});
+
+downLeftBtn.addEventListener("click", function () {
+   
+    let topLeftY = window.getComputedStyle(topLeftImage).getPropertyValue("background-position-y");
+    let topLeftX = window.getComputedStyle(topLeftImage).getPropertyValue("background-position-x");
+    let addPixelY = parseInt(topLeftY, 10) + 5 + "px";
+    let addPixelX = parseInt(topLeftX, 10) - 5 + "px";
+    topLeftImage.style.backgroundPositionY = addPixelY;
+    topLeftImage.style.backgroundPositionX = addPixelX;
+
+    let topRightY = window.getComputedStyle(topRightImage).getPropertyValue("background-position-y");
+    let topRightX = window.getComputedStyle(topRightImage).getPropertyValue("background-position-x");
+    let addPixelY1 = parseInt(topRightY, 10) + 5 + "px";
+    let addPixelX1 = parseInt(topRightX, 10) - 5 + "px";
+    topRightImage.style.backgroundPositionY = addPixelY1;
+    topRightImage.style.backgroundPositionX = addPixelX1;
+
+    let bottomLeftY = window.getComputedStyle(bottomLeftImage).getPropertyValue("background-position-y");
+    let bottomLeftX = window.getComputedStyle(bottomLeftImage).getPropertyValue("background-position-x");
+    let addPixelY2 = parseInt(bottomLeftY, 10) + 5 + "px";
+    let addPixelX2 = parseInt(bottomLeftX, 10) - 5 + "px";
+    bottomLeftImage.style.backgroundPositionY = addPixelY2;
+    bottomLeftImage.style.backgroundPositionX = addPixelX2;
+
+    let bottomRightY = window.getComputedStyle(bottomRightImage).getPropertyValue("background-position-y");
+    let bottomRightX = window.getComputedStyle(bottomRightImage).getPropertyValue("background-position-x");
+    let addPixelY3 = parseInt(bottomRightY, 10) + 5 + "px";
+    let addPixelX3 = parseInt(bottomRightX, 10) - 5 + "px";
+    bottomRightImage.style.backgroundPositionY = addPixelY3;
+    bottomRightImage.style.backgroundPositionX = addPixelX3;
+  
+});
+
+downRightBtn.addEventListener("click", function () {
+   
+    let topLeftY = window.getComputedStyle(topLeftImage).getPropertyValue("background-position-y");
+    let topLeftX = window.getComputedStyle(topLeftImage).getPropertyValue("background-position-x");
+    let addPixelY = parseInt(topLeftY, 10) + 5 + "px";
+    let addPixelX = parseInt(topLeftX, 10) + 5 + "px";
+    topLeftImage.style.backgroundPositionY = addPixelY;
+    topLeftImage.style.backgroundPositionX = addPixelX;
+
+    let topRightY = window.getComputedStyle(topRightImage).getPropertyValue("background-position-y");
+    let topRightX = window.getComputedStyle(topRightImage).getPropertyValue("background-position-x");
+    let addPixelY1 = parseInt(topRightY, 10) + 5 + "px";
+    let addPixelX1 = parseInt(topRightX, 10) + 5 + "px";
+    topRightImage.style.backgroundPositionY = addPixelY1;
+    topRightImage.style.backgroundPositionX = addPixelX1;
+
+    let bottomLeftY = window.getComputedStyle(bottomLeftImage).getPropertyValue("background-position-y");
+    let bottomLeftX = window.getComputedStyle(bottomLeftImage).getPropertyValue("background-position-x");
+    let addPixelY2 = parseInt(bottomLeftY, 10) + 5 + "px";
+    let addPixelX2 = parseInt(bottomLeftX, 10) + 5 + "px";
+    bottomLeftImage.style.backgroundPositionY = addPixelY2;
+    bottomLeftImage.style.backgroundPositionX = addPixelX2;
+
+    let bottomRightY = window.getComputedStyle(bottomRightImage).getPropertyValue("background-position-y");
+    let bottomRightX = window.getComputedStyle(bottomRightImage).getPropertyValue("background-position-x");
+    let addPixelY3 = parseInt(bottomRightY, 10) + 5 + "px";
+    let addPixelX3 = parseInt(bottomRightX, 10) + 5 + "px";
+    bottomRightImage.style.backgroundPositionY = addPixelY3;
+    bottomRightImage.style.backgroundPositionX = addPixelX3;
   
 });
 
